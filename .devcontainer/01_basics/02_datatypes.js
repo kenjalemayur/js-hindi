924fe750820f562536566cb6ee67494fab183136
kenjalemayur <123161380+kenjalemayur@users.noreply.github.com>
2024-07-06 05:29:24 +0000

    devcontainer

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer vrsion
+
+//alert (3+3) //we are using node js not browser.
+
+ console.log(3+
+    3); // code readability shoukd be heigh.
+
+    //number 
+    //bigInt
+    //null
+    //boolean
+    //string
+    //symbol
+    //undefined
---
+++ .devcontainer/01_basics/test.js
@@ -0,0 +1,15 @@
+const accountId = 12334
+let accountEmail = "mayur@gam.com"
+var accountPassword ="1373"
+accountCity = "medgha"
+ let accountstate;
+
+
+
+
+accountEmail = "fhfvh@ganul.com"
+accountPassword = "85680"
+accountCity = "satara"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountstate])
